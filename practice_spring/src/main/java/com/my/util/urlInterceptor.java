8f73dcc744fb0281affa1e1eaed08844ce3fe0e6
sangwon0724 <leews0724@naver.com>
2021-01-30 17:52:39 +0900

    little repair

--- practice_spring/src/main/java/com/my/util/urlInterceptor.java
+++ practice_spring/src/main/java/com/my/util/urlInterceptor.java
@@ -15,11 +15,11 @@ public class urlInterceptor extends HandlerInterceptorAdapter {
         HttpSession session = request.getSession();
         Object user = session.getAttribute("user");
          
-        if ( user ==null ){
+        if ( user == null ){
             // 로그인이 안되어 있는 상태임으로 로그인 폼으로 다시 돌려보냄(redirect)
         	session.setAttribute("result", "need");
             response.sendRedirect("/user/login");
-            return false; // 더이상 컨트롤러 요청으로 가지 않도록false로 반환함
+            return false; //더이상 컨트롤러 요청으로 가지 않도록false로 반환함
         }
          
         //preHandle의 return => 컨트롤러 요청 uri로의 이동에 대한 허가 여부
@@ -29,9 +29,7 @@ public class urlInterceptor extends HandlerInterceptorAdapter {
  
     // 컨트롤러가 수행되고 화면이 보여지기 직전에 수행되는 메서드
     @Override
-    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
-            ModelAndView modelAndView) throws Exception {
-        // TODO Auto-generated method stub
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
         super.postHandle(request, response, handler, modelAndView);
     }
 }
